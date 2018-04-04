545c56b1dae49a6bccfe20827e710372ee0ae2c2
Miguel Bento <masbento@gmail.com>
2018-04-04 23:46:02 +0100

    Add YAML support

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,21 @@ extern crate structopt;
 
 extern crate human_size;
 extern crate rand;
+extern crate yaml_rust;
 
+use std::iter::empty;
+use std::str::FromStr;
+use std::collections::HashSet;
 use std::ffi::OsString;
-use std::fs::DirEntry;
+use std::fs::File;
 use std::io::Result;
+use std::io::prelude::*;
 use std::path::PathBuf;
 
-use human_size::Size;
+use human_size::{Size, Multiple};
 use rand::{thread_rng, seq};
 use structopt::StructOpt;
+use yaml_rust::{Yaml, YamlLoader};
 
 /// Filmilhoes - random pick files
 #[derive(StructOpt, Debug)]
@@ -23,28 +29,80 @@ struct Opt {
     directory: PathBuf,
 
     /// Set max number of files to pick
-    #[structopt(short = "n", long = "number", default_value = "5")]
-    n: usize,
+    #[structopt(short = "n", long = "number")]
+    n: Option<usize>,
     
     /// Minimum size of files to pick
-    #[structopt(short = "s", long = "size", default_value = "100MB")]
-    min_size: Size,
+    #[structopt(short = "s", long = "size")]
+    min_size: Option<Size>,
 
     /// Directories to exclude
     #[structopt(short = "x", long = "exclude", parse(from_os_str))]
     exclude: Vec<OsString>,
 }
 
+struct Settings {
+    directory: PathBuf,
+    n: usize,
+    min_size: Option<u64>,
+    exclude: HashSet<OsString>
+}
 
-fn main() {
+const DEFAULT_YAML: &'static str = "files: 1";
+
+fn get_settings() -> Settings {
     let opt = Opt::from_args();
 
-    let min_size = opt.min_size.into_bytes() as u64;
-    let files = get_files(&opt.directory, min_size, &opt.exclude);
+    let yaml_path = opt.directory.join(".filmilhoes.yml");
+    let yamls = if let Ok(mut f) = File::open(yaml_path) {
+        let mut buffer = String::new();
+        f.read_to_string(&mut buffer).expect("Failed to read yaml file!");
+        YamlLoader::load_from_str(&buffer).expect("Invalid yaml file!")
+    } else {
+        YamlLoader::load_from_str(&DEFAULT_YAML).unwrap()
+    };
+
+    let yaml = &yamls[0];
+
+    Settings {
+        directory: opt.directory,
+        n: opt.n.unwrap_or(
+            match yaml["files"] {
+                Yaml::Integer(i) if i > 0 => i as usize,
+                Yaml::BadValue            => 1usize,
+                _                         => panic!("YAML: 'files' must be a positive number")
+            }),
+        min_size: opt.min_size.or(
+            match yaml["minsize"] {
+                Yaml::String(ref size) => Some(Size::from_str(size).expect("YAML: 'minsize' must be a file size")),
+                Yaml::BadValue     => None,
+                _                  => panic!("YAML: 'minsize' must be a file size")
+            })
+            .map(| s | s.into_bytes() as u64),
+        exclude: opt.exclude.into_iter()
+            .chain(
+                match yaml["exclude"] {
+                    Yaml::Array(ref array) => array.iter().map(|e| {
+                        match e {
+                            &Yaml::String(ref s) => OsString::from(s),
+                            _ => panic!("YAML: Invalid 'exclude' item")
+                        }
+                    }).collect::<Vec<_>>(),
+                    Yaml::BadValue   => empty::<OsString>().collect::<Vec<_>>(),
+                    _ => panic!("YAML: 'exclude' is a list of strings")
+                }
+            )
+            .collect()
+    }
+}
+
+fn main() {
+    let settings = get_settings();
+    let files = get_files(&settings);
 
     let mut rng = thread_rng();
-    let sample = if files.len() >= opt.n {
-        seq::sample_slice(&mut rng, &files, opt.n)
+    let sample = if files.len() >= settings.n {
+        seq::sample_slice(&mut rng, &files, settings.n)
     } else {
         files
     };
@@ -57,7 +115,7 @@ fn main() {
     }
 }
 
-static SCALES: &'static [&str] = &["B", "KiB", "MiB", "GiB", "TiB"];
+static SCALES: &'static [Multiple] = &[Multiple::Byte, Multiple::Kibibyte, Multiple::Mebibyte, Multiple::Gigibyte, Multiple::Tebibyte];
 
 fn format_size(size: u64) -> String {
     let mut scale = 0usize;
@@ -72,21 +130,26 @@ fn format_size(size: u64) -> String {
     format!("{:7.2} {:>3}", scalled, SCALES[scale])
 }
 
-fn get_files(directory: &PathBuf, min_size: u64, exclude: &[OsString]) -> Vec<PathBuf> {
+fn get_files(settings: &Settings) -> Vec<PathBuf> {
+    let directory = &settings.directory;
+
     if !directory.is_dir() {
         return Vec::new();
     }
     
     let mut vec = Vec::with_capacity(1000);
     {
-        let mut check_file = |file_entry: &DirEntry| {
-            if let Ok(metadata) = file_entry.metadata() {
-                if metadata.len() >= min_size {
-                    vec.push(file_entry.path())
+        let r = if let Some(size) = settings.min_size {
+            visit_dirs(directory, &settings.exclude, &mut |file_path| {
+                let len = get_file_len(&file_path).expect("Failed to get file length");
+                if len >= size {
+                    vec.push(file_path);
                 }
-            }
+            })
+        } else {
+            visit_dirs(directory, &settings.exclude, &mut |file_path| vec.push(file_path))
         };
-        visit_dirs(directory, exclude, &mut check_file).expect("Failed to search directory");
+        r.expect("Failed to search directory");
     }
     vec
 }
@@ -96,7 +159,7 @@ fn get_file_len(path: &PathBuf) -> Result<u64> {
     Ok(metadata.len())
 }
 
-fn visit_dirs(dir: &PathBuf, exclude: &[OsString], cb: &mut FnMut(&DirEntry)) -> Result<()> {
+fn visit_dirs(dir: &PathBuf, exclude: &HashSet<OsString>, cb: &mut FnMut(PathBuf)) -> Result<()> {
     if dir.is_dir() {
         for entry in dir.read_dir()? {
             let entry = entry?;
@@ -107,7 +170,7 @@ fn visit_dirs(dir: &PathBuf, exclude: &[OsString], cb: &mut FnMut(&DirEntry)) ->
                     visit_dirs(&path, exclude, cb)?;
                 }
             } else {
-                cb(&entry);
+                cb(path);
             }
         }
     }
